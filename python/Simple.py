56fd02b69c7091501df0404ad811b37de24a336c
changejava <changejava@gmail.com>
2015-01-25 22:37:23 +0800

    add comments to examples

--- python/Simple.py
+++ python/Simple.py
@@ -1,5 +1,3 @@
-import math
-
 print 1 + 1
 print 1 - 1
 print 1 * 2
@@ -7,6 +5,7 @@ print 1 / 2
 print 1.0 / 2.0
 print 10 % 3
 print 2 ** 3
+# force cal integer division
 print 1.0 // 2
 print 100000000000000000000000000000000000000000000L
 print 0xAF
@@ -30,8 +29,10 @@ print x
 x = 'hello world'
 
 print x
+# string len
 print len(x)
 
+# if else statement
 the_world_is_flag = 0
 if the_world_is_flag:
     print 'the world is flag'
@@ -43,7 +44,7 @@ else:
 x = 100
 y = 100
 print x + y
-
+# if elseif else statement
 if x == y:
     print 'x eq y'
 elif x > y:
@@ -53,16 +54,18 @@ elif x < y:
 else:
     print 'x:' + x + ",y:" + y
 print pow(2, 4)
-
+# python language none
 x = None
 
 print x
-
+# abs
 print abs(-19)
 print round(1.5 / 0.2)
+# python object
 x = object()
-
+# python object hashcode
 print x.__hash__()
+# import math function
 import math
 
 print math.floor(32.9)
@@ -74,25 +77,79 @@ print '"dont\"t "care"'
 print '"dont\"t "care"\nnew Line'
 word = "help" + 'a'
 print word
+# substring word index 1
 print word[1]
+# substring from 0 to 4
 print word[0:4]
 print word
+# five twice word
 word = '<' + word * 5 + '>'
 print word
 import cmath
 
 cmath.sqrt(-1)
 
-# x = raw_input("please input your name :")
+# result is contains single quotes
 x = repr("changejava")
 print 'hello ' + x + "!"
 
 print repr("hello world!")
-
+# result is simple
 print str("hello world")
-
+# convert long to longL
 print repr(100000000000000)
 print str(100000000000000)
 
 temp = 43
+# if not contains repr ,will error
 print "the temperature is " + repr(temp)
+# x = input("x:")
+# y = raw_input("please input your name :")
+#
+#
+# print x
+# print y
+# convert string object to int
+x = int('12')
+# convert string object to long
+y = long("123444")
+# convert string object to float
+z = float("111")
+print x
+print y
+
+print r"c:\\tools\\code\\gs-python"
+# x = int(raw_input("please input an number")) raw input
+#
+# if x < 0:
+# x = 0
+# print 'negative changed to zero'
+# elif x == 0:
+# print 'zero'
+# elif x == 1:
+# print 'single'
+# else:
+# print 'More'
+# help()
+# unicode support
+print u'Hello\u0020World !'
+# python list
+a = ['change', 'java', 10, 100]
+# iterator list
+print a
+# list item 0
+print a[1]
+# list len or size()
+print len(a)
+# set index 1 is c#
+a[1] = 'c#'
+
+print a[1]
+# iterator list items
+print a
+# print a[4] linux index out of range
+# list length -2
+print a[-2]
+# set  list length-2 item value
+a[-2] = 200
+print a[-2]
